d9a3e143f07d899e1776b1b7542c86bb091f5e03
Ahahahaha69 <56919834+Ahahahaha69@users.noreply.github.com>
2019-10-23 18:37:04 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$channelAccessToken = 'CHANNEL ACCESS TOKEN';
+$channelAccessToken = 6ljayYvU2XYkNXeTJfkHGTKhNMs3ZRY/m7/iupewvYwSevHZrg7Icghbn4olimqNu2H8T69Ioqb0fb2LMRWqOMnLU6h9vLAgwWRwkDda02SENh4L7j/CCq9aFtjiHJDfGhpvYB1xEAwNH6TnaOidlAdB04t89/1O/w1cDnyilFU=;
 
-$channelSecret = 'CHANNEL SECRET';
+$channelSecret = 393aa460d866e2571976fccdf96b5eee;
